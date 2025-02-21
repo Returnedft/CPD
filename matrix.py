241f9a3343d22563c54fed35479b6a58963cd9a0
Lucas Faria <lucasfaria@outlok.pt>
2025-02-21 16:53:57 +0000

    Added the second algorithm in python

--- matrix.py
+++ matrix.py
@@ -3,21 +3,9 @@ import time
 def OnMult(m_ar : int, m_br : int) -> None:
     temp = 0
 
-    pha = []
-    phb = []
-    phc = []
-
-    for i in range(0, m_ar):
-        for j in range(0, m_ar):
-            pha.append(1.0)
-    
-    for i in range(0, m_br):
-        for j in range(0, m_br):
-            phb.append(i + 1)
-    
-    for i in range(0, m_ar):
-        for j in range(0, m_ar):
-            phc.append(0)
+    pha = [1.0 for i in range(m_ar) for j in range(m_ar)]
+    phb = [i + 1 for i in range(m_br) for j in range(m_br)]
+    phc = [0 for i in range(m_ar) for j in range(m_ar)]
     
     time1 = time.time()
 
@@ -41,6 +29,26 @@ def OnMultLine(m_ar, m_br):
     '''
     Line x line matriz multiplication
     '''
+
+    pha = [1.0 for i in range(m_ar) for j in range(m_ar)]
+    phb = [i + 1 for i in range(m_br) for j in range(m_br)]
+    phc = [0 for i in range(m_ar) for j in range(m_ar)]
+    
+    time1 = time.time()
+
+    for i in range(0, m_ar):
+        for j in range(0, m_br):
+            for k in range(0, m_ar):
+                phc[i * m_ar + k] += pha[i*m_ar + j] * phb[j*m_br + k]
+    
+    time2 = time.time()
+
+    final = time2 - time1
+
+    print(f'Final Time is {final}')
+
+    for i in range(0, min(10,m_br)):
+        print(phc[i])
     return None
 
 def OnMultBlock(m_ar, m_br, bksize) :
